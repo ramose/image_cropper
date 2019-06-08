68b94e3c49bc99edbd386506405eaec93a1d4340
ramose <leopapilaya@gmail.com>
2019-06-08 16:38:14 +0800

    Implement tflite load model

--- lib/camera3.dart
+++ lib/camera3.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:io' as io;
 // import 'dart:ui';
 import 'package:image_picker/image_picker.dart';
 import 'package:simple_permissions/simple_permissions.dart';
-// import 'package:image_cropper/image_cropper.dart';
 import 'package:image/image.dart' as Img;
-// import 'package:extended_image_library/extended_image_library.dart';
+
+import 'package:tflite/tflite.dart';
 
 enum PhotoSource {
   camera,
@@ -59,13 +60,58 @@ class _Camera3PageState extends State<Camera3Page> {
     }
 
     if (image != null) {
-      Img.Image img = Img.decodeImage(image.readAsBytesSync());
-      Img.Image crop = Img.copyCrop(img, 0, 0, 100, 100);
-      image.writeAsBytesSync(Img.encodePng(crop));
+      // Img.Image img = Img.decodeImage(image.readAsBytesSync());
+      // Img.Image crop = Img.copyCrop(img, 0, 0, 100, 100);
+      // image.writeAsBytesSync(Img.encodePng(crop));
       setState(() {
         _image = image;
       });
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _loadModelAndLabels();
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future _loadModelAndLabels() async {
+    Tflite.close();
+    String res;
+
+    try {
+      res = await Tflite.loadModel(
+          model: "assets/mobilenet_v1_1.0_224.tflite",
+          labels: "assets/labels.txt",
+          numThreads: 1 // defaults to 1
+          );
+      print(res);
+    } on PlatformException {
+      print('xxxxxx>> Failed to load model.');
+    }
+  }
+
+  _detect(io.File image) async {
+    var recognitions = await Tflite.detectObjectOnImage(
+        path: image.path, // required
+        model: "YOLO",
+        imageMean: 0.0,
+        imageStd: 255.0,
+        threshold: 0.3, // defaults to 0.1
+        numResultsPerClass: 2, // defaults to 5
+        // anchors: anchors, // defaults to [0.57273,0.677385,1.87446,2.06253,3.33843,5.47434,7.88282,3.52778,9.77052,9.16828]
+        blockSize: 32, // defaults to 32
+        numBoxesPerBlock: 5, // defaults to 5
+        asynch: true // defaults to true
+        );
+    print('---> detect : $recognitions');
   }
 
   @override
@@ -117,12 +163,12 @@ class _Camera3PageState extends State<Camera3Page> {
               child: Text('Add Picture'),
               onPressed: showPhotoOptions,
             ),
-            // RaisedButton(
-            //   child: Text('Crop'),
-            //   onPressed: () {
-            //     _cropImage(_image);
-            //   },
-            // ),
+            RaisedButton(
+              child: Text('Detect'),
+              onPressed: () {
+                _detect(_image);
+              },
+            ),
           ],
         ),
       ),
